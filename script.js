da8a5e5990d8806db3be8de67f445c34705147ca
Carlos Victor <cvictorrm08@gmail.com>
2023-05-26 14:50:04 -0300

    fix: corrigindo script

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-var idade = 22
-
-if (idade < 16) {
-    console.log(`Não vota, pois tem ${idade} anos`)
-} else if (idade < 18) {
-        console.log(`Voto opcional, pois tem ${idade} anos`)
-    } else if (idade < 65) {
-        console.log(`Voto obrigatório, pois é maior de idade`)
+var resultado = window.document.getElementById('res')//recebe a div#res
+function verificar() {
+    var idade = Number(window.document.getElementById('ipti').value)//recebe o valor do input idade, convertido em number
+    if (idade == 0) {
+        window.alert(`Por gentileza, insira uma idade!`)
+    } else if (idade == 1) {
+        resultado.innerHTML = `<p>Com ${idade} ano você não pode votar</p>`
+    } else if (idade > 1 && idade < 16) {
+        resultado.innerHTML = `<p>Com ${idade} anos você não pode votar</p>`
+    } else if (idade >= 18 && idade < 71) {
+        resultado.innerHTML = `<p>Com ${idade} anos o voto é obrigatório</p>`
     } else {
-        console.log(`Voto opcional, pois tem 65 anos ou mais`)
+        resultado.innerHTML = `<p>Com ${idade} anos o voto é opcional</p>`
     }
+}
